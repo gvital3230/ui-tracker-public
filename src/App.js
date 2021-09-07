56af0309e30d7b80a4eea0e82809881467554272
Vitalii Goncharov <1c.audit@gmail.com>
2021-09-07 09:00:27 +0300

    add simple websocket client + browser identification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import ReconnectingWebSocket from 'reconnecting-websocket';
+import FingerprintJS from '@fingerprintjs/fingerprintjs'
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const rws = new ReconnectingWebSocket('ws://localhost:8080/echo');
+        let timer = null
+
+        rws.addEventListener('open', () => {
+            // Initialize an agent at application startup.
+            const fpPromise = FingerprintJS.load();
+            (async () => {
+                // Get the visitor identifier when you need it.
+                const fp = await fpPromise
+                const result = await fp.get()
+
+                // This is the visitor identifier:
+                const visitorId = result.visitorId
+                timer = setInterval(() => {
+                    rws.send('hello from ' + visitorId + '!')
+                }, 1000);
+            })()
+        });
+
+        return () => {
+            if (timer) {
+                clearInterval(timer)
+            }
+        }
+    }, []);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
